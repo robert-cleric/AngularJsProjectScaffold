83cd7c7ff301c87686d0658cae9f4374c36c2926
Robert Cleric <robert.cleric@gmail.com>
2018-03-28 19:19:52 -0400

    Added some configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": ["angular", "eslint:recommended"],
+    "rules": {
+        "quotes": ["error", "single", {"avoidEscape" : true}],
+        "semi": ["error", "always"]
+    }
+};
